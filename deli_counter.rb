a0775287222e4994cd1369b7e4a341791b7f9ea8
John Wojciechowski <jwojo13@gmail.com>
2017-11-13 20:19:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deil, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.index} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
 end
 
 
